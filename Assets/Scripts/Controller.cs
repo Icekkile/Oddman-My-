cdda5f32257870eef3780caba9d1052d332a7d42
Icekkile <iceman.ryt@gmail.com>
2019-09-17 23:34:42 +0300

    Initial commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public PlayerMovement pm;
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+        Vector2 pointToMove = ControllerInput();
+        pm.DetermineForce(pointToMove);
+        pm.Move(pointToMove);
+    }
+
+    Vector2 ControllerInput ()
+    {
+        return (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float maxForceMagitude;
+    public float forceParam;
+    private float force;
+
+    public void DetermineForce (Vector2 pointToMove)
+    {
+        if (pointToMove.magnitude >= maxForceMagitude)
+            force = maxForceMagitude * forceParam;
+        else
+            force = pointToMove.magnitude * forceParam;
+    }
+
+    public void Move(Vector2 pointToMove)
+    {
+        rb.velocity = pointToMove.normalized * force;
+        //rb.AddForce(pointToMove.normalized * force);
+    }
+}
