ac4db8c9d6fd2e4a6eed4781d2eb7e522864841f
Icekkile <iceman.ryt@gmail.com>
2019-09-21 14:22:57 +0300

    normal dash created

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class Controller : MonoBehaviour , IController
 {
     public PlayerMovement pm;
 
     private void Update()
     {
-        float temp = 0;
-        if (Input.GetMouseButtonDown(0) || temp > 0)
+        if (Input.GetMouseButton(0))
         {
-            pm.rb.gravityScale = 0;
             Vector2 pointToMove = ControllerInput();
-            temp = pm.Move(pointToMove);
-            return;
+            SetTargetPoint(pointToMove);
         }
-        pm.rb.gravityScale = 1;
+
+
+    }
+
+    public void SetTargetPoint(Vector2 TargetPoint)
+    {
+        pm.SetTargetPoint(TargetPoint);
     }
 
     Vector2 ControllerInput ()
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,52 +5,71 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float maxForceDistance;
     public float Speed;
 
-    private float force;
+    public float MinTargetSpeed, MaxTargetSpeed;
 
-    private void Start()
+    private Vector2? _targetPoint;
+    public Vector2? targetPoint
     {
+        get => _targetPoint;
+        private set
+        {
+            _targetPoint = value;
 
+            if(value != null)
+            {
+                rb.gravityScale = 0;
+            }
+            else
+            {
+                rb.gravityScale = 1;
+                rb.velocity = Vector2.zero;
+            }
+        }
     }
+
+    private float force;
+    private float tolerance = 0.01f;
 
     public void DetermineForce (Vector2 pointToMove)
     {
-        if (Hypotenuse(pointToMove, rb.transform.position) >= maxForceDistance)
-            force = maxForceDistance * Speed;
-        else
-            force = Hypotenuse(pointToMove, rb.transform.position) * Speed;
+        float distanceToPoint = Vector2.Distance(pointToMove, (Vector2)rb.transform.position);
+
+        force = Mathf.Clamp(distanceToPoint * Speed, MinTargetSpeed, MaxTargetSpeed);
     }
 
-    public float Move(Vector2 pointToMove)
+    public void SetTargetPoint (Vector2 targetPointParam)
     {
-        //distance from this point to the point we need. Indicator.
-        float temp = Hypotenuse(rb.transform.position, pointToMove);
-        //
-        Debug.Log(temp);
-        //if dash is all
-        if (temp <= 0)
+        targetPoint = targetPointParam;
+    }
+
+    private void Update()
+    {
+        if (targetPoint != null)
         {
-            rb.velocity = Vector2.zero;
-            Debug.Log(rb.velocity);
-            return temp;
+            GoToPointProcess();
         }
-        //
+    }
+
+    private void GoToPointProcess()
+    {
+        if ((targetPoint.Value - (Vector2)rb.transform.position).magnitude > rb.velocity.magnitude * 0.02f)
+        {
+            Move();
+        }
+        else
+        {
+            targetPoint = null;
+        }
+    }
+
+    public void Move()
+    {
+        Vector2 currentTargetPoint = targetPoint.Value;
 
         //moving
-        DetermineForce(pointToMove);// - method that ditermines value of force from point we need.
-        rb.velocity = pointToMove.normalized * force * Time.deltaTime;
-        //
-
-        return temp;
-    }
-
-    float Hypotenuse (Vector2 point_1, Vector2 point_2)
-    {
-        float result = 0;
-        float x = Mathf.Abs(point_1.x - point_2.x);
-        float y = Mathf.Abs(point_1.y - point_2.y);
-        return result = Mathf.Sqrt(x * x + y * y);
+        DetermineForce(currentTargetPoint);// - method that ditermines value of force from point we need.
+        rb.velocity = (currentTargetPoint - (Vector2)rb.transform.position).normalized * force;
     }
 }
