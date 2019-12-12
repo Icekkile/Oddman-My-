09bdb10ad63cb32106a292dcb2173187cf919997
Icekkile <iceman.ryt@gmail.com>
2019-12-13 00:20:41 +0200

    Create and Join Battle button created
    and some refactoring

---
+++ Assets/Scripts/Networking/MatchMaker.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class MatchMaker : NetworkManager
+{
+    public static MatchMaker ins;
+
+    public int MaxPosiblePlayers;
+    public NetworkMatch _matchMaker;
+
+    private AvailableMatches _refAvailableMatches;
+
+    void Start()
+    {
+        ins = this;
+        singleton.StartMatchMaker();
+        _matchMaker = singleton.matchMaker;
+        _refAvailableMatches = new AvailableMatches();
+    }
+
+
+
+    //call this method to request a match to be created on the server
+    public void CreateInternetMatch()
+    {
+        _matchMaker.CreateMatch("Knockouters", 4, true, "", "", "", 0, 0, OnInternetMatchCreate);
+    }
+
+    //this method is called when your request for creating a match is returned
+    private void OnInternetMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        if (!success)
+            return;
+
+        MatchInfo hostInfo = matchInfo;
+
+        UIController.ins.SetActive(false);
+        singleton.StartHost(hostInfo);
+    }
+
+
+
+    //this method is called when a list of matches is returned
+    public void ConnectInternetMatch()
+    {
+        List<MatchInfoSnapshot> matches = _refAvailableMatches.GetAvailableMatches();
+
+        if (matches.Count == 0)
+            return;
+        
+        MatchInfoSnapshot connectMatch = matches[Random.Range(0, matches.Count)];
+
+        _matchMaker.JoinMatch(connectMatch.networkId, "", "", "", 0, 0, OnJoinInternetMatch);
+        
+
+    }
+
+    //this method is called when your request to join a match is returned
+    private void OnJoinInternetMatch(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        if (success)
+        {
+            MatchInfo hostInfo = matchInfo;
+            UIController.ins.SetActive(false);
+
+
+            singleton.StartClient(hostInfo);
+        }
+    }
+}
