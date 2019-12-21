db8ce622e4617fce2e34f8a88a8b9c26bd4ee025
Icekkile <iceman.ryt@gmail.com>
2019-12-21 02:15:26 +0200

    Scenes and bugged translation between created

--- Assets/Scripts/UI/ButtonHandlers.cs
+++ Assets/Scripts/UI/ButtonHandlers.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking.Match;
+using UnityEngine.SceneManagement;
 
 public class ButtonHandlers : MonoBehaviour
 {
     public void PlayBattleButtonHandler ()
     {
+        SceneManager.LoadScene("Battle");
         MatchMaker.ins.PlayInternetMatch();
     }
 }
