12dc8c5a9462fa354b58a44a9d6c53c4bc254557
Icekkile <iceman.ryt@gmail.com>
2019-12-27 14:35:12 +0200

    Created preparing before End Scene Translation

--- Assets/Scripts/BattleData.cs
+++ Assets/Scripts/BattleData.cs
@@ -1,15 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum MatchResults
+{
+    Win,
+    Lose,
+    Draw,
+}
 
 public class BattleData : MonoBehaviour
 {
     public static BattleData ins;
 
     public Body ClientPlayer;
+    public MatchResults matchResult;
 
     private void Awake()
     {
         ins = this;
+    }
+
+
+    public void EndBattleClearing()
+    {
+        ClientPlayer = null;
+
     }
 }
