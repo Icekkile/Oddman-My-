3f1db6534d4a9300adb89db0c6894220dddab830
Icekkile <iceman.ryt@gmail.com>
2020-01-24 16:22:24 +0200

    local Profiles and profile saves created

---
+++ Assets/Scripts/Profiling/TrophiesSaver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrophiesSaver : MonoBehaviour, ISaver<int>
+{
+    public int param { get; set; }
+
+    public void LoadParam()
+    {
+        param = PlayerPrefs.GetInt("Trophies");
+    }
+
+    public void SaveParam()
+    {
+        PlayerPrefs.SetInt("Trophies", param);
+    }
+}
